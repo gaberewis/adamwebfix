a323beeca00441653f902f4deaf5531171e62bbb
gaberewis <gaber.ewis@gmail.com>
2026-01-29 10:53:04 +0300

    until search and favicon change

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useSubmit, Form } from "react-router-dom";
 import { useContext, createContext } from "react";
 import { ProductContainer } from '../components';
 import { useDashboardContext } from "./Dashboard";
@@ -7,21 +7,48 @@ import { useDashboardContext } from "./Dashboard";
 const ProductsContext = createContext();
 
 const Allproducts = () => {
+  const submit = useSubmit();
   const { data, searchValues } = useLoaderData();
+  const { search } = searchValues
   const { totalproducts } = data;
   const { logoutUser, user } = useDashboardContext();
+
+
+    const debounce = (onChangeFunc) => {
+        let timeOute;
+        return (e) => {
+            const formToSubmit = e.currentTarget.form;
+            clearTimeout(timeOute);
+            timeOute = setTimeout(() => {
+                onChangeFunc(formToSubmit);
+            }, 2000)
+
+        }
+
+    };
+
+
 
   return (
 
     <ProductsContext.Provider value={{ data, searchValues }}>
+      <Form className='form' action={`/dashboard?search=${search}`}>
+           <input
+                            type='search'
+                            name='search'
+                            defaultValue={search}
+                            onChange={debounce((form)=> submit(form))}
+                        />
+      </Form>
+
       <button className="btn"><Link to='/dashboard/add-product' >Add product</Link></button>
-      <input type="search" name='search' id='search' defaultValue='a' />
+
       <h5> {totalproducts} product{totalproducts > 1 ? 's' : ''} found</h5>
-      {user &&   (<><button className="btn" onClick={logoutUser}>logout</button>
-      <h1>{user?.name}</h1></>)}
-    
+      {user && (<><button className="btn" onClick={logoutUser}>logout</button>
+        <h1>{user?.name}</h1></>)}
+
       <ProductContainer />
-     
+
     </ProductsContext.Provider>
 
   )
@@ -30,3 +57,4 @@ const Allproducts = () => {
 export const useproductsContext = () => useContext(ProductsContext);
 
 export default Allproducts;
+
