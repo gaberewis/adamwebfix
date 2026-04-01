d1efa522d5baecd6d48d5c5cb879c630c899ef33
gaberewis <gaber.ewis@gmail.com>
2026-04-01 14:14:09 +0300

    land page bones

---
+++ client/src/css-pocket/Home.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+
+
+const CssSTL = styled.main`
+
+color : green;
+
+
+
+`;
+
+export default CssSTL;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,54 @@
-const Home = ()=>{
-    return  <h2>Adam Web Fix</h2>
+import CssSTL from "../css-pocket/Home";
+import { FaCcPaypal } from "react-icons/fa";
+import { CiBank } from "react-icons/ci";
+import { SiWesternunion } from "react-icons/si";
+import { RiWhatsappFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+const Home = () => {
+    <CssSTL>
+        <div className="head-part">
+            <img src="/public/coloredlogo.png" alt="colored-logo" />
+            return  <h3>AdamWebFix</h3>
+
+            <p>
+We are interested in rebuilding and innovating your website to better reflect your business proficiency and quality.
+Contact us to build or improve your website and get a fast, clear, and modern solution.
+ We focus on delivering a polished design that enhances your brand and creates a strong, professional online presence.
+            </p>
+        </div>
+
+        <div className="contact">
+            <h4>Contact details</h4>
+            <p>
+                <span>
+                    <RiWhatsappFill />
+                    <a href="tel:+96597264747" style={{ textDecoration: "none", color: "inherit" }}>
+                        00965-97264747
+                    </a>
+                </span>
+            </p>
+
+            <p>
+                <span>
+                    <MdEmail />
+                    <a
+                        href="mailto:gaber.ewis@gmail.com"
+                        style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                        gaber.ewis@gmail.com
+                    </a>
+                </span>
+            </p>
+
+        </div>
+
+        <div className="payment">
+            <h4>Payment options</h4>
+            <span><FaCcPaypal /></span>
+            <span><CiBank /></span>
+            <span><SiWesternunion /></span>
+        </div>
+    </CssSTL>
 }
 
 export default Home;
