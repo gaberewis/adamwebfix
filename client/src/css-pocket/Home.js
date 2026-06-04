4ce31afe1b8318f88142b86bdd041b58553ab3d8
gaberewis <gaber.ewis@gmail.com>
2026-06-04 14:12:18 +0300

    instant page design

--- client/src/css-pocket/Home.js
+++ client/src/css-pocket/Home.js
@@ -3,28 +3,89 @@ import styled from "styled-components";
 const CssSTL = styled.main`
 
   padding: 2rem;
-  margin: 0 auto;
-  min-height: 100vh;
+  margin: 0 auto
   max-width: 90vw;
-  
 
+  
+  hr{
+  margin : 2rem 2rem;
+  }
   .intro {
    
     margin-bottom: 2rem;
     img {
       max-width : 40px; 
     }
-h1, h5, p {
+h1, h5 {
  text-align : center;
-margin : 2rem 0 2rem 0;
+margin : 2rem 0 1rem 0;
+line-height : 2;
+}
+p {
+text-align : center;
 }
 
 a {
   text-decoration: underline;
 }
-    p {
-      margin : 1rem 0 2rem 0;
-    }
+    
+  }
+
+  .featurs{
+  margin : 0 auto;
+  max-width : 900px;
+  display: flex;
+
+  div{
+  border : 1px solid var(--grey-400);
+  border-radius : var(--border-radius);
+  padding : 2.5rem 1.8rem;
+  margin : 2rem 1rem;
+  max-width : 400px;
+  }
+ 
+  p, h5 {
+   text-align : left;
+   margin-bottom : 1rem
+   
+   }
+   h5{
+   text-decoration : underline;
+   }
+
+  }
+
+
+ .start  {
+ 
+ a{
+ text-decoration : none;
+ }
+ 
+ button {
+  max-width : 400px;
+  margin : 2rem  auto ;
+  font-size: 1.5rem;
+  padding : .3rem 3rem;
+  border-radius : 3rem;
+  text-align : center;
+  box-shadow : var(--shadow-1);
+   display : block;
+  }
+
+  button:hover{
+  color : var(--grey-800);
+  background : var(--grey-300);
+  box-shadow : none;
+  }
+ }
+
+
+
+  @media(max-width : 798px){
+
+  .featurs{
+  display: block }
   }
 
 `;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Form, Link} from "react-router-dom";
+import { Form, Link } from "react-router-dom";
 import { FormRow, SubmitButton } from "../components";
 import CssSTL from "../css-pocket/Home";
 
@@ -8,8 +8,8 @@ const Home = () => {
     return (
         <CssSTL>
             <div className="intro">
-            <Link to="/">
-                <img src="/logo.png" alt="colored-logo" />
+                <Link to="/">
+                    <img src="/logo.png" alt="colored-logo" />
                 </Link>
                 <h1>Adam Web Fix</h1>
 
@@ -18,15 +18,13 @@ const Home = () => {
 
                     From concept to execution, we redesign and optimize websites to deliver clarity, performance, and impact — ensuring your online presence stands out in a competitive market.
 
-                    Partner with us to create a website that not only looks exceptional but performs exceptionally. Try our new service and launch your online business in minutes with <Link to="/instantpage" >Instant-Page.</Link>
-                    
+                    Partner with us to create a website that not only looks exceptional but performs exceptionally. Try our new service and launch your online business in minutes with <Link to="/instantpage" >Instant-Page</Link>
+
                 </p>
-                <h5 className="cta">Contact us today and take your website to the next level</h5>
+               
 
                 <hr />
-
-
-
+                 <h5 className="cta">Contact us today and take your website to the next level</h5>
 
                 <Form method="post" className="form">
 
