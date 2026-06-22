d17472e700fd2a354eb1e19adba1becdc7c3ea23
gaberewis <gaber.ewis@gmail.com>
2026-06-22 10:47:35 +0300

    atr

--- controllers/pages.js
+++ controllers/pages.js
@@ -4,7 +4,7 @@ import Pages from "../models/Pages.js";
 export const capturePayment = async (req, res) => {
   try {
     const { orderId, userId } = req.body;
-console.log(userId);
+
     const auth = await axios.post(
       "https://api-m.sandbox.paypal.com/v1/oauth2/token",
       "grant_type=client_credentials",
