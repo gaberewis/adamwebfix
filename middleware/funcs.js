445b6af4f495fe59bcb676cf1203e7155537942e
gaberewis <gaber.ewis@gmail.com>
2026-08-17 10:10:38 +0300

    ag2

--- middleware/funcs.js
+++ middleware/funcs.js
@@ -64,14 +64,20 @@ export const authenticateAdmin = (...roles) => {
 
 
 
+
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
