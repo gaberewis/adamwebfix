69e4c4e6b83476dcfaac6bae7131f043358dd16b
gaberewis <gaber.ewis@gmail.com>
2026-09-04 17:08:56 +0300

    page done

--- client/src/css-pocket/page.jsx
+++ client/src/css-pocket/page.jsx
@@ -14,6 +14,7 @@ const Stl = styled.main`
 
   .content {
     margin: 0 auto;
+    margin-bottom : .2rem;
     width: 90vw;
     max-width: 992px;
     padding: 3rem 1.6rem;
@@ -35,28 +36,27 @@ color : var(--red);
    
 
 
-  .slide {
-    display: flex;
-    flex-direction: column;
-    height : 400px;
-    align-items: center;
-    gap: 1.8rem;
-  }
+.slide {
+  height: 320px;
+  display: flex;
+  justify-content: center; /* vertical */
+  align-items: center;     /* horizontal */
+}
 
-  .slide img {
-    display: block;
-    width: 90%;
-    max-width: 385px;
-    max-height : 385px;
-    margin: 0 auto;
-  }
+.slide img {
+  display: block;
+  width: 90%;
+  max-width : 420px;
+  max-height: 100%;
+  object-fit: contain;
+}
 
-  .dots {
-   margin-top : 2rem;
-    display: flex;
-    justify-content: center;
-    gap: 1rem;
-  }
+.dots {
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
 
   .dots span {
     cursor: pointer;
@@ -77,7 +77,7 @@ color : var(--red);
   .price {
     margin-top: .8rem;
     display: flex;
-    gap: 2rem;
+    gap: 1rem;
     font-weight: 600;
     font-size: 1.4rem;
     color: var(--pure-green);
@@ -109,21 +109,97 @@ color : var(--red);
      margin-bottom : 1.3rem;
      }
 
-      .spec-items{
-      
-       display : flex;
-      gap : 3rem;
-      background-color : var(--grey-100);
-      padding : 1rem;
+    .spec-items{
+       width : 75VW;
+        max-width : 485px;
+        margin-top : .2rem;
+        gap : 2.5rem;
+        display: flex;
+       justify-content: space-between;
+       background-color: var(--grey-100);
+       padding: 1rem;
     }
+       .spec-items span{
+       width : 50%;
+       }
+    
+
     
     }
     // end of spec
 
+    .order{
+    cursor : pointer;
+    margin : 3.5rem auto 0 ;
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    gap : .4rem;
+    font-size : 1.8rem;
+    font-weight : 500;
+    color : var(--pure-green);
+  
+    }
+
+     .order span:nth-child(2){
+     font-size : 3rem;
+     }
+
+  .show-more {
+  margin-top: 3rem;
+  padding: 1.6rem;
+  width :100%;
+
+  .more {
+    margin-top: 1.6rem;
+    display: flex;
+    gap: 2rem;
+    padding : 1rem;
+
+    overflow-x: auto;
+    overflow-y: hidden;
+
+    .price span:nth-child(1) {
+    color : var(--red);
+    } 
+
+  img {
+  width: 80px;
+  height: 80px;
+  max-width: 80px;
+  flex-shrink: 0;
+
+}
+
+    div {
+      font-size: 0.825rem;
+      flex-shrink: 0;
+    }
+  }
+}
+   
+    //  end of show more
   
 
       }
     // end of content
+
+.contact {
+background-color: var(--grey-600);
+color : var(--grey-50);
+display : flex;
+flex-direction : column;
+gap : 1.6rem;    
+padding : 2.3rem;
+border-top : solid 4px var(--grey-700);
+}
+
+.contact p{
+max-width : 385px;
+
+}
+
+
 
 `;
 
--- client/src/pages/Page.jsx
+++ client/src/pages/Page.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 import Stl from "../css-pocket/page";
 import { FaCircleDot } from "react-icons/fa6";
+import { MdEmail, MdLocationOn } from "react-icons/md";
+import { RiWhatsappFill, RiPhoneFill } from "react-icons/ri";
 
 
 
@@ -14,9 +17,9 @@ const Page = () => {
         "/not-found.svg",
         "/logo.png"];
 
-        const specification = [{"color" : "red"},{"size" : "large"},{"status" : "new"}];
+    const specification = [{ "color": "red" }, { "size": "large" }, { "status": "new" }];
 
-        
+
 
     return (
         <Stl>
@@ -25,9 +28,8 @@ const Page = () => {
                 <div className="head">
                     <div className="slide">
                         <img src={imageSlide[current]} alt="slide" />
-
-
                     </div>
+
                     <div className="dots">
                         {
                             imageSlide.map((item, index) => (
@@ -44,42 +46,88 @@ const Page = () => {
 
                 </div>
 
-                   <div className="details">
-                        <h5>Product Details:</h5>
-                        <p>
-                            "Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                <div className="details">
+                    <h5>Product Details:</h5>
+                    <p>
+                        "Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-                        </p>
+                    </p>
+
+                </div>
+
+                <div className="spec">
+                    <h5>Specification </h5>
+
+                    {specification.map((item, index) => {
+
+                        const [key, value] = Object.entries(item)[0];
+
+                        return (
+
+                            <div className="spec-items" key={index}>
+                                <span>{key}</span>
+                                <span>{value}</span>
+                            </div>
+
+                        )
+                    })
+
+                    }
+                </div>
+
+
+                <div
+                    className="order"
+                    onClick={() =>
+                        window.open(`https://wa.me/96597264747`, "_blank")
+                    }
+                >
+                    <span>Order Now</span>
+                    <span><RiWhatsappFill /></span>
+                </div>
+
+                <div className="show-more">
+                    <h5>More products: </h5>
+
+                    <div className="more">
+
+                        {
+                            imageSlide.map((item, index) => {
+                                return (
+                                    <Link to={'#'} >
+                                     <div key={index}><img src={item} alt='more-product-image' />
+                                        <div className="name">Product Name</div>
+                                        <div className="price"><span><del>200KD</del></span> <span>160KD</span></div>
+
+                                    </div>
+                                    </Link>
+                                   
+                                )
+
+                            })
+                        }
+
 
                     </div>
-                   
-                    <div className="spec">
-                         <h5>Specification </h5>
 
-{specification.map((item, index)=> {
 
-    const[key, value]= Object.entries(item)[0];
+                </div>
 
-return(
-    
-    <div className="spec-items" key={index}>
-    <span>{key}</span>
-    <span>{value}</span>
-     <hr />
-    </div>
-   
-)
-})
 
-}
-                  
-                    </div>
 
+            </div>
+
+            <div className="contact">
+
+                <p><span>< RiPhoneFill /> </span> <span>Phone: 000000000000</span></p>
+                <p><span>< RiWhatsappFill /> </span><span>WhatsApp: 0000000000 </span></p>
+                <p><span>< MdEmail /> </span><span>Email: email@send.com</span></p>
+                <p><span>< MdLocationOn /> </span><span>Address: physical business location to be located in here</span></p>
 
             </div>
 
