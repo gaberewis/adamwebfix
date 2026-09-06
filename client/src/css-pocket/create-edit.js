ead2dad5b856db35943bb1c41a3eb72ae5ef75a3
gaberewis <gaber.ewis@gmail.com>
2026-09-06 20:01:50 +0300

    finish create page css

--- client/src/css-pocket/create-edit.js
+++ client/src/css-pocket/create-edit.js
@@ -7,8 +7,46 @@ const Stl = styled.main`
 
 margin : 0 auto;
 margin-top : 2rem;
-
 width : 90vw;
+
+
+.p-spec p:first-child{
+margin-bottom : 1rem;
+}
+
+.spec { 
+position : relative;
+display : flex;
+gap : 1.2rem;
+margin-bottom : 1rem;
+
+.form-input {
+min-width : 150px;
+border-radius : 10px;
+}
+
+span{
+ color : var(--red);
+ position : absolute;
+ bottom : 2.5rem;
+ left : .5rem;
+cursor : pointer;
+}
+
+}
+//end of spec
+
+.add-spec {
+  display: flex;
+ align-items: center;
+  gap: 0.3rem;
+}
+.add-spec span:nth-child(2){
+color :var(--pure-green);
+margin-top : .5rem;
+cursor :pointer;
+}
+
 
 
 `;
--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -3,6 +3,7 @@ import { FormRow, FormRowSelect, SubmitButton, currencies } from '../components'
 import Stl from '../css-pocket/create-edit';
 import { useState } from 'react';
 import { RiDashboardFill, RiAddBoxFill } from "react-icons/ri";
+import { TiDelete } from "react-icons/ti";
 
 
 
@@ -11,7 +12,7 @@ import { RiDashboardFill, RiAddBoxFill } from "react-icons/ri";
 const CreatePage = () => {
 
 
-    const [showDiscount, setDiscount] = useState(false);
+
     const [specifications, setSpecifications] = useState([]);
     const orderContact = ['phone', 'whatsapp', 'email'];
 
@@ -27,68 +28,110 @@ const CreatePage = () => {
             ];
         });
     };
+
+    const deleteSpec = (i) => {
+        setSpecifications(prev =>
+            prev.filter((_, index) => index !== i)
+        );
+    };
+
     return (<Stl>
         <Link to="/dashboard" ><RiDashboardFill /> dashboard</Link>
 
         <Form method="post" className='form' >
             <FormRow type='file' name='images' labelText={'Add product images'} accept='images/*' multiple required />
-            <FormRow type='text' name="company" labelText={'Enter company or business name'} maxLength={100} required />
+            <FormRow type='text' name="company" labelText={'company / business name'} maxLength={100} required />
             <FormRow type='text' name="name" labelText={'product Name'} maxLength={100} required />
-
             <FormRow type='text' labelText={'Product price'} maxLength={20} required />
-            <FormRow type='checkbox' labelText={'have a product discount?'}
-                onChange={() => setDiscount(pre => !pre)}
-            />
-            {showDiscount &&
-                <FormRow type='text' labelText={'enter the price before'} maxLength={20} />
-            }
+            <FormRow type='text' labelText={'price before discount (optional)'} maxLength={20} />
             <FormRowSelect name='currency' list={currencies} />
-
             <textarea className='form-textarea' name="details" labelText={'Enter product details'} maxLength={1000}
                 placeholder="Enter product description..."
                 required ></textarea>
-
-
             <FormRow type='text' mame='phone' labelText={'Phone number'} />
             <FormRow type='text' mame='whatsapp' labelText={'Wahts app  number'} />
             <FormRow type='email' mame='email' labelText={'email'} />
-            <FormRowSelect name='order' list={orderContact} labelText={'How do you prefer receiving order'} />
-
+            <FormRowSelect name='order' list={orderContact} labelText={'recieve order by: '} />
             <textarea className='form-textarea' name="address" maxLength={3000}
                 placeholder="Add business address optional..."
             ></textarea>
 
+            <div className='p-spec'>
+                <p>Add product specification (optional)</p>
+                <div className='spec'>
+                    <FormRow
+                        name={`spec`}
+                        labelText="Specification"
+                        maxLength={100}
+                    />
+                    <textarea
+                        name={`details`}
+                        labelText="Details"
+                        className='form-textarea'
+                        maxLength={300}
+                        placeholder='specification details'
+                    ></textarea>
+                </div>
 
-            <div>
+                <div className='spec'>
+                    <FormRow
+                        name={`spec`}
+                        labelText="Specification"
+                        maxLength={100}
+                    />
+                    <textarea
+                        name={`details`}
+                        labelText="Details"
+                        className='form-textarea'
+                        maxLength={300}
+                        placeholder='specification details'
+                    ></textarea>
+
+                </div>
+                <div className='spec'>
+                    <FormRow
+                        name={`spec`}
+                        labelText="Specification"
+                        maxLength={100}
+                    />
+                    <textarea
+                        name={`details`}
+                        labelText="Details"
+                        className='form-textarea'
+                        maxLength={300}
+                        placeholder='specification details'
+                    ></textarea>
+
+                </div>
                 {specifications.map((item, index) => (
                     <div key={index}>
-                        <FormRow
-                            name={`specifications[${index}].spec`}
-                            labelText="Specification"
-                            maxLength={100}
-                        />
-                        <FormRow
-                            name={`specifications[${index}].details`}
-                            labelText="Details"
-                            maxLength={100}
-                        />
-                        <FormRow
-                            name={`specifications[${index}].spec`}
-                            labelText="Specification"
-                            maxLength={100}
 
-                        />
-                        <FormRow
-                            name={`specifications[${index}].details`}
-                            labelText="Details"
-                            maxLength={100}
-                        />
+                        <div className='spec'>
+                            <span onClick={() => deleteSpec(index)} ><TiDelete size={22} /></span>
+
+                            <FormRow
+                                name={`specifications[${index}].spec`}
+                                labelText="Specification"
+                                maxLength={100}
+                            />
+                            <textarea
+                                name={`specifications[${index}].details`}
+                                labelText="Details"
+                                className='form-textarea'
+                                maxLength={300}
+                                placeholder='specification details'
+                            ></textarea>
+
+                        </div>
+
                     </div>
 
                 ))}
+
                 {
-                    specifications.length < 10 ? <p>{specifications.length > 0 ? 'Add more specification' : 'Add product specification'} <span onClick={addSpecification}>
-                        <RiAddBoxFill size={25} /></span></p> : ""
+                    specifications.length < 7 ? <p className='add-spec'>
+                        <span>Add more</span> <span onClick={addSpecification}>
+                            <RiAddBoxFill size={22} /></span></p> : ""
 
                 }
             </div>
